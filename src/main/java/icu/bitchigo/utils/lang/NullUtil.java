c452202b3aa021a71f2d24dbe7f8fd8c9b8b753a
iko233 <ln1020142782@outlook.com>
2022-05-27 14:06:05 +0800

    添加判空工具

---
+++ src/main/java/icu/bitchigo/utils/lang/NullUtil.java
@@ -0,0 +1,47 @@
+package icu.bitchigo.utils.lang;
+
+/**
+ * null工具类
+ *
+ * @author iko233
+ * @date 2022/05/27
+ */
+public class NullUtil {
+    /**
+     * 是否为null
+     */
+    public static boolean isNull(Object obj) {
+        return obj == null;
+    }
+    /**
+     * 是否存在null
+     */
+    public static boolean isExistNull(Object...obj) {
+        for (Object o : obj) {
+            if (isNull(o)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    /**
+     * 是否不为null
+     */
+    public static boolean isNotNull(Object obj) {
+        return !isNull(obj);
+    }
+
+    /**
+     * 是否都不为null
+     */
+    public static boolean isAllNotNull(Object...obj) {
+        for (Object o : obj) {
+            if (isNull(o)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
