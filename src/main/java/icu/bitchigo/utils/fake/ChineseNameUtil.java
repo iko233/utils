dd3fed21889b35ae002ab74b03b6364241f502be
iko233 <ln1020142782@outlook.com>
2022-05-31 15:18:33 +0800

    删除中文人名工具类无效代码

--- src/main/java/icu/bitchigo/utils/fake/ChineseNameUtil.java
+++ src/main/java/icu/bitchigo/utils/fake/ChineseNameUtil.java
@@ -293,11 +293,4 @@ public class ChineseNameUtil {
     public static String randomName() {
         return randomSurname() + randomPreName();
     }
-
-    public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
-            System.out.println(randomName());
-        }
-    }
-
 }
